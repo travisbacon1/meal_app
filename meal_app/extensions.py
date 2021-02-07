5de93d28efcd39d5f55b0db06a011528c9135c55
Travis Bacon <travisbacon1@gmail.com>
2021-02-07 19:34:35 +0000

    Updated venvActivate and README

--- meal_app/extensions.py
+++
@@ -1,6 +0,0 @@
-# from flask_mysqldb import MySQL
-# # from app import app
-
-# # mysql = MySQL()
-
-# mysql = MySQL(app)
--- meal_app/meal_plans/create.py
+++ meal_app/meal_plans/create.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
-from .. import mysql, variables
+from .. import mysql
 import json
 
 create = Blueprint('create', __name__, template_folder='templates', static_folder='static')
