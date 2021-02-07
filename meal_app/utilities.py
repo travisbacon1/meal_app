cd2960a74a8817ba41d4d2b1ae0244dc52f72040
Travis Bacon <travisbacon1@gmail.com>
2021-02-07 21:48:27 +0000

    Added docstring

--- meal_app/utilities.py
+++ meal_app/utilities.py
@@ -1,6 +1,15 @@
 from . import mysql
 
 def execute_mysql_query(query_string):
+    """Executes a MySQL query
+    Parameters
+    -------
+    query_string: string\n
+
+    Returns
+    ------
+    result: tuple
+    """
     db_cursor = mysql.connection.cursor()
     db_cursor.execute(query_string)
     results = db_cursor.fetchall()
