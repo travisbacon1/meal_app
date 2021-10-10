b9f5aac82cfd87ff22e062bc6bdb8852d63a62ac
Travis Bacon <travisbacon1@gmail.com>
2021-10-10 19:08:21 +0100

    Added .get()

--- meal_app/meals/find.py
+++ meal_app/meals/find.py
@@ -27,18 +27,18 @@ def some_meal_page(meal):
         db_cursor.execute(query)
         result = db_cursor.fetchall()
         location_details = {}
-        if result[0]['Website'] == None or result[0]['Website'] == '':
-            location_details['Book'] = result[0]['Book']
-            location_details['Page'] = result[0]['Page']
+        if result[0].get('Website') == None or result[0].get('Website') == '':
+            location_details['Book'] = result[0].get('Book')
+            location_details['Page'] = result[0].get('Page')
         else:
-            location_details['Website'] = result[0]['Website']
+            location_details['Website'] = result[0].get('Website')
         fresh_ingredients = [list(json.loads(result[0]['Fresh_Ingredients']).keys()), list(json.loads(result[0]['Fresh_Ingredients']).values())]
         tinned_ingredients = [list(json.loads(result[0]['Tinned_Ingredients']).keys()), list(json.loads(result[0]['Tinned_Ingredients']).values())]
         dry_ingredients = [list(json.loads(result[0]['Dry_Ingredients']).keys()), list(json.loads(result[0]['Dry_Ingredients']).values())]
         dairy_ingredients = [list(json.loads(result[0]['Dairy_Ingredients']).keys()), list(json.loads(result[0]['Dairy_Ingredients']).values())]
         return render_template('find_results.html', meal_name=meal,
                                 location_details = location_details, location_keys = location_details.keys(),
-                                staple = result[0]['Staple'],
+                                staple = result[0].get('Staple'),
                                 len_fresh_ingredients = len(fresh_ingredients[0]), fresh_ingredients_keys=fresh_ingredients[0], fresh_ingredients_values=fresh_ingredients[1],
                                 len_tinned_ingredients = len(tinned_ingredients[0]), tinned_ingredients_keys=tinned_ingredients[0], tinned_ingredients_values=tinned_ingredients[1],
                                 len_dry_ingredients = len(dry_ingredients[0]), dry_ingredients_keys=dry_ingredients[0], dry_ingredients_values=dry_ingredients[1],
