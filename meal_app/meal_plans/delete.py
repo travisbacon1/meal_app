2b6597cc0e7ed96c257639c695ce825e12162632
Travis Bacon <travisbacon1@gmail.com>
2021-11-06 20:13:35 +0000

    Code simplifications in delete meal plan code

--- meal_app/meal_plans/delete.py
+++ meal_app/meal_plans/delete.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template, request
+from glob import glob
 import os
 
 delete = Blueprint('delete', __name__, template_folder='templates', static_folder='../static')
 
-def delete_plans(meal_plan_list):
+def delete_plans(meal_plan_list) -> None:
     """Deletes the selected meal plans from the local saved_meal_plans directory
     
     Parameters
@@ -21,15 +22,12 @@ def delete_plans(meal_plan_list):
 
 @delete.route('/delete', methods=['GET', 'POST'])
 def delete_meal_plan():
-    from glob import glob
     meal_plans = [f.replace('.json', '').split('/')[1] for f in glob("saved_meal_plans/*.json")]
     if len(meal_plans) == 0:
         return render_template('no_meal_plans.html')
     if request.method == "POST":
-        details = request.form
-        details_dict = details.to_dict()
+        details_dict = request.form.to_dict()
         meal_plans_to_delete = [value for key, value in details_dict.items() if 'Meal Plan' in key]
         delete_plans(meal_plans_to_delete)
         return render_template('delete_complete.html')
-    return render_template('delete.html',
-                            len_meal_plans = len(meal_plans), meal_plans = meal_plans)
+    return render_template('delete.html', meal_plans = meal_plans)
