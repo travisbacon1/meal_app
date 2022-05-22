4ca2d30918e43aeedcba11efb5524dbeee20db52
Travis Bacon <travisbacon1@gmail.com>
2022-05-22 16:34:42 +0100

    Removed unneeded code

--- database_setup/import_database.py
+++ database_setup/import_database.py
@@ -12,26 +12,26 @@ database = mysql.connector.connect(
     database=os.environ['MYSQL_DATABASE']
     )
 
-# query_string = """CREATE TABLE IF NOT EXISTS `MealsTableNew` (
-#   `Name` varchar(45) NOT NULL,
-#   `Staple` varchar(45) DEFAULT NULL,
-#   `Book` varchar(45) DEFAULT NULL,
-#   `Page` varchar(45) DEFAULT NULL,
-#   `Website` varchar(45) DEFAULT NULL,
-#   `Fresh_Ingredients` json DEFAULT NULL,
-#   `Tinned_Ingredients` json DEFAULT NULL,
-#   `Dry_Ingredients` json DEFAULT NULL,
-#   `Dairy_Ingredients` json DEFAULT NULL,
-#   `Last_Made` date DEFAULT NULL,
-#   `Spring_Summer` tinyint DEFAULT NULL,
-#   `Autumn_Winter` tinyint DEFAULT NULL,
-#   `Quick_Easy` tinyint DEFAULT NULL,
-#   `Special` tinyint DEFAULT NULL,
-#   PRIMARY KEY (`Name`)
-# ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci"""
+query_string = f"""CREATE TABLE IF NOT EXISTS `{os.environ['MYSQL_TABLE']}` (
+  `Name` varchar(45) NOT NULL,
+  `Staple` varchar(45) DEFAULT NULL,
+  `Book` varchar(45) DEFAULT NULL,
+  `Page` varchar(45) DEFAULT NULL,
+  `Website` varchar(45) DEFAULT NULL,
+  `Fresh_Ingredients` json DEFAULT NULL,
+  `Tinned_Ingredients` json DEFAULT NULL,
+  `Dry_Ingredients` json DEFAULT NULL,
+  `Dairy_Ingredients` json DEFAULT NULL,
+  `Last_Made` date DEFAULT NULL,
+  `Spring_Summer` tinyint DEFAULT NULL,
+  `Autumn_Winter` tinyint DEFAULT NULL,
+  `Quick_Easy` tinyint DEFAULT NULL,
+  `Special` tinyint DEFAULT NULL,
+  PRIMARY KEY (`Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci"""
 
-# db_cursor = database.cursor()
-# db_cursor.execute(query_string)
+db_cursor = database.cursor()
+db_cursor.execute(query_string)
 
 # query_string = """set global local_infile=true;"""
 # db_cursor = database.cursor()
@@ -40,7 +40,7 @@ database = mysql.connector.connect(
 with open(f"../full_database_backup3.json","r") as f:
     for line in f:
         line = json.loads(line.replace(',\n','').replace(']','').replace('[',''))
-        query_string = f"INSERT IGNORE INTO {os.environ['MYSQL_DATABASE']}.MealsTableNew(Name, Staple, Book, Page, Website, Fresh_Ingredients, Tinned_Ingredients, Dry_Ingredients, Dairy_Ingredients, Last_Made, Spring_Summer, Autumn_Winter, Quick_Easy, Special) VALUES (\"{line['Name']}\", \"{line['Staple']}\", \"{line['Book']}\", \"{line['Page']}\", \"{line['Website']}\", \'{json.dumps(line['Fresh_Ingredients'])}\', \'{json.dumps(line['Tinned_Ingredients'])}\', \'{json.dumps(line['Dry_Ingredients'])}\', \'{json.dumps(line['Dairy_Ingredients'])}\', \'{line['Last_Made']}\', \'{line['Spring_Summer']}\', \'{line['Autumn_Winter']}\', \'{line['Quick_Easy']}\', \'{line['Special']}\')"
+        query_string = f"INSERT IGNORE INTO {os.environ['MYSQL_DATABASE']}.{os.environ['MYSQL_TABLE']}(Name, Staple, Book, Page, Website, Fresh_Ingredients, Tinned_Ingredients, Dry_Ingredients, Dairy_Ingredients, Last_Made, Spring_Summer, Autumn_Winter, Quick_Easy, Special) VALUES (\"{line['Name']}\", \"{line['Staple']}\", \"{line['Book']}\", \"{line['Page']}\", \"{line['Website']}\", \'{json.dumps(line['Fresh_Ingredients'])}\', \'{json.dumps(line['Tinned_Ingredients'])}\', \'{json.dumps(line['Dry_Ingredients'])}\', \'{json.dumps(line['Dairy_Ingredients'])}\', \'{line['Last_Made']}\', \'{line['Spring_Summer']}\', \'{line['Autumn_Winter']}\', \'{line['Quick_Easy']}\', \'{line['Special']}\')"
         print(query_string)
         db_cursor = database.cursor()
         db_cursor.execute(query_string)
--- wsgi.py
+++ wsgi.py
@@ -1,33 +1,14 @@
-# import sys
-# sys.path.append('meal_app')
-
-# from app import create_app as application
-
-# app = application()
-
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0')
-
 import os
 import sys
 sys.path.append('meal_app')
 
-# from bla import app as application
 from meal_app.app import create_app
 from dotenv import load_dotenv
 
-project_folder = os.path.expanduser('~/meal_app')  # adjust as appropriate
+project_folder = os.path.expanduser('~/meal_app')
 load_dotenv(os.path.join(project_folder, '.env'))
 
 app = create_app()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-
-# from meal_app.app import create_app
-
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# app = create_app()
