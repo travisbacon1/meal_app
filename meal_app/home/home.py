983dde04bac0c067843b8d21b75589f65278c842
Travis Bacon <travisbacon1@gmail.com>
2022-06-26 23:32:51 +0100

    Removed comments

--- meal_app/home/home.py
+++ meal_app/home/home.py
@@ -18,23 +18,4 @@ def index():
     if request.method == "POST":
         url = request.form['submit'].lower().replace(' ', '_')
         return redirect(url_for(f'{url}.main'))
-
-        # if request.form['submit'] == 'Add Ingredient':
-        #     return redirect(url_for('add_ingredient.main'))
-        # elif request.form['submit'] == 'Add Meal':
-        #     return redirect(url_for('add_meal.main'))
-        # elif request.form['submit'] == 'Edit Meal':
-        #     return redirect(url_for('edit_meal.main'))
-        # elif request.form['submit'] == 'Get Meal Info':
-        #     return redirect(url_for('find_meal.main'))
-        # elif request.form['submit'] == 'Search Ingredients':
-        #     return redirect(url_for('search_ingredients.main'))
-        # elif request.form['submit'] == 'List Meals':
-        #     return redirect(url_for('list_meals.main'))
-        # elif request.form['submit'] == 'Create Meal Plan':
-        #     return redirect(url_for('create_meal_plan.main'))
-        # elif request.form['submit'] == 'Load Meal Plan':
-        #     return redirect(url_for('load_meal_plan.main'))
-        # elif request.form['submit'] == 'Delete Meal Plan':
-        #     return redirect(url_for('delete_meal_plan.main'))
     return render_template('index.html', buttons=buttons)
