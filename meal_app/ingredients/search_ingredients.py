7c1bd39b849caf71e19f6cfd52316111e1174081
Travis Bacon <travisbacon1@gmail.com>
2022-07-09 20:49:25 +0100

    Refactored search_ingredients code to use sqlmodel

--- meal_app/ingredients/search_ingredients.py
+++ meal_app/ingredients/search_ingredients.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
-from ..utilities import execute_mysql_query
-import os
-from models import IngredientsTable
+from models import MealsTable, IngredientsTable
 from app import engine
 from sqlmodel import Session, select, func
+
 
 search_ingredients = Blueprint('search_ingredients', __name__, template_folder='templates', static_folder='../static')
 
@@ -20,19 +19,17 @@ def main():
         for key, value in details_dict.items():
             if value != "null":
                 json_key, ingredient = key, value
-        query_string = f"""SELECT * FROM {os.environ['MYSQL_DATABASE']}.{os.environ['MYSQL_TABLE']}
-                WHERE JSON_EXTRACT({json_key}, '$."{ingredient}"');"""
-        results = execute_mysql_query(query_string)
-        session['meal_list'] = [result['Name'] for result in results]
-        return redirect(url_for('search_ingredients.search_results', ingredient = ingredient))
-    return render_template('search_ingredients.html', 
-                            ingredient_types = ["Fresh", "Dairy", "Dry", "Tinned"], ingredients=unpacked_results)
+        with Session(engine) as sql_session:
+            statement = select(MealsTable.Name).where(func.json_extract(getattr(MealsTable, json_key), f'$."{ingredient}"'))
+            session['meal_list'] = sql_session.exec(statement).fetchall()
+        return redirect(url_for('search_ingredients.search_results', ingredient=ingredient))
+    return render_template('search_ingredients.html', ingredients=unpacked_results)
 
 
 @search_ingredients.route('/search_ingredients/<ingredient>', methods=['GET', 'POST'])
 def search_results(ingredient):
     if request.method == "GET":
-        meals = session.pop('meal_list', [])
-        return render_template('search_results.html', ingredient = ingredient, len_meals = len(meals), meals = meals)
+        meals = session.pop('meal_list')
+        return render_template('search_results.html', ingredient=ingredient, meals=meals)
     else:
         return redirect(url_for('search.index'))
